1099ea111a81ff204b8e76eb1b88cba2a90dd03b
rusik <rusyaaa566@gmail.com>
2025-12-20 19:28:54 +0400

    добавил ходьбу

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Level.h"
+#include <conio.h>
 
 int main()
 {
@@ -9,7 +10,57 @@ int main()
 	int playerX = level.playerStartX;
 	int playerY = level.playerStartY;
 
-	level.drawWithPlayer(playerX, playerY);
+	while (true)
+	{
+		system("cls");
+		level.drawWithPlayer(playerX, playerY);
+
+		char input = _getch();
+		if (input == 'q')
+		{
+			break;
+		}
+		
+		int xx = 0, yy = 0;
+		if (input == 'w')
+		{
+			yy = -1;
+		}
+		if (input == 's')
+		{
+			yy = 1;
+		}
+		if (input == 'a')
+		{
+			xx = -1;
+		}
+		if (input == 'd')
+		{
+			xx = 1;
+		}
+
+		int newX = playerX + xx;
+		int newY = playerY + yy;
+
+		if (newX < 0 || newX >= level.getWidth())
+		{
+			continue;
+		}
+		if (newY < 0 || newY >= level.getHeight()) 
+		{ 
+			continue;
+		}
+
+		if (level.getTile(newX, newY) == '#')
+		{
+			continue;
+		}
+
+		playerX = newX;
+		playerY = newY;
+	}
+
+	
 
 	return 0;
 }
